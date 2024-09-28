4ea3fbd29d57d2c6051bf1fc317b0be945e5c496
Snowflakes* <119450371+AbdurrAli@users.noreply.github.com>
2024-09-28 07:45:52 +0700

    commit ke-5 finished the project for intent apps

--- app/src/main/java/com/example/intentapps/MainActivity.kt
+++ app/src/main/java/com/example/intentapps/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.intentapps
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.provider.Telephony.Mms.Intents
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var tvDataResult: TextView
+
+    /*
+    Untuk membuat activity yang dapat memberikan nilai kita perlu membuat
+    ActivityResultLauncher terlebih dahulu
+    */
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+            result -> if (result.resultCode == MoveForResultActivity.RESULT_CODE && result.data != null) {
+        val selectedValue = result.data?.getStringExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE) ?: ""
+        tvDataResult.text = getString(R.string.result_text, selectedValue)
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,6 +41,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveWithObject: Button = findViewById(R.id.btn_change_activity_with_objek)
         btnMoveWithObject.setOnClickListener(this)
+
+        val btnDialNumber: Button = findViewById(R.id.btn_dial_number)
+        btnDialNumber.setOnClickListener(this)
+
+        val btnMoveForResult: Button = findViewById(R.id.btn_move_with_result)
+        btnMoveForResult.setOnClickListener(this)
+
+        tvDataResult = findViewById(R.id.tv_data_result)
+
     }
 
     override fun onClick(v: View?) {
@@ -51,6 +77,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
                 startActivity(moveWithObjectIntent)
             }
+
+            R.id.btn_dial_number -> {
+                val phoneNumber = "087777102284"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
+            }
+
+            R.id.btn_move_with_result -> {
+                val moveForResultIntent = Intent(this@MainActivity, MoveForResultActivity::class.java)
+                resultLauncher.launch(moveForResultIntent)
+            }
+
         }
     }
 }
